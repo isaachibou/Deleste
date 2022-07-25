0d271f1eb5c9aedaa142359858f9d9ad626ffee0
Isaac <isaac.hibou@gmail.com>
2022-07-25 23:26:49 +0200

    première custo

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="w-24 h-24 relative rounded-full block mx-auto border-2 border-cyan-100">
+        <Image
+          src="/images/profile.jpg"
+          alt="ma tête à yellowstone" 
+          layout="fill" // required
+          objectFit="cover" // change to suit your needs
+          className="rounded-full" // just an example
+        />
+      </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
